14f35fc94cdd76511798c86d8dd51f5503931f72
MatthewMattei <96449493+MatthewMattei@users.noreply.github.com>
2024-05-22 16:45:54 -0700

    Updated disk namespace; specifically open_file, disk_file, and disk classes + update .gitignore

--- include/redstone/disk/disk.hpp
+++ include/redstone/disk/disk.hpp
@@ -1,24 +1,59 @@
 #pragma once
 
+#include <algorithm>
 #include <cstddef>
 #include <cstdint>
+#include <memory>
 #include <mutex>
 #include <span>
+#include <stdint.h>
 #include <sys/_types/_ssize_t.h>
+#include <unistd.h>
 #include <unordered_map>
 #include <vector>
 
 namespace redstone::disk {
-class file {
+
+class disk_file {
 public:
-  size_t read(std::uint64_t pos, std::span<std::byte> buf);
-  size_t write(std::uint64_t pos, std::span<const std::byte> buf);
-  int saveBuffer();
-  void setState(bool state);
+  int64_t read(std::uint64_t pos, std::span<std::byte> buf);
+  int64_t write(std::uint64_t pos, std::span<const std::byte> buf);
+  size_t get_size();
 
 private:
   std::mutex mutex_;
   std::vector<std::byte> buffer_;
-  bool open_;
 };
+
+class open_file {
+
+  struct write_req {
+    std::vector<std::byte> bytes;
+    uint64_t pos;
+  };
+
+public:
+  int64_t read(std::span<std::byte> buf);
+  int64_t write(std::span<const std::byte> buf);
+  int64_t pread(std::span<std::byte> buf, size_t offset);
+  int64_t pwrite(std::span<const std::byte> buf, size_t offset);
+  int64_t lseek(size_t offset, int whence);
+  void flush_backlog();
+
+private:
+  std::vector<write_req> write_backlog_;
+  std::mutex mutex_;
+  std::shared_ptr<disk_file> disk_;
+  uint64_t pos_;
+  bool direct_;
+  int64_t pread_unlocked(std::span<std::byte> buf, size_t offset);
+  int64_t pwrite_unlocked(std::span<const std::byte> buf, size_t offset);
+};
+
+class disk {
+public:
+private:
+  std::unordered_map<std::string, std::shared_ptr<disk_file>> filenameMap;
+};
+
 } // namespace redstone::disk
--- src/disk/disk.cpp
+++ src/disk/disk.cpp
@@ -1,16 +1,22 @@
 #include "redstone/disk/disk.hpp"
+#include <_types/_uint64_t.h>
+#include <cstddef>
 #include <fstream>
 #include <iostream>
 #include <mutex>
+#include <sys/_types/_int64_t.h>
+#include <sys/_types/_seek_set.h>
+#include <utility>
+#include <vector>
 
 namespace redstone::disk {
 
-// File Functions
+// disk_file functions
 
-size_t file::write(std::uint64_t pos, std::span<const std::byte> buf) {
+int64_t disk_file::write(std::uint64_t pos, std::span<const std::byte> buf) {
   std::lock_guard<std::mutex> lock(mutex_);
-  if (!this->open_)
-    throw std::invalid_argument("File not open.");
+  if (pos < 0)
+    return -22; // Invalid arg
   // Expand buffer if more space is needed
   if (pos + buf.size_bytes() > buffer_.size()) {
     // Calculate the new size needed
@@ -26,45 +32,98 @@ size_t file::write(std::uint64_t pos, std::span<const std::byte> buf) {
   return buf.size_bytes();
 }
 
-size_t file::read(std::uint64_t pos, std::span<std::byte> buf) {
+int64_t disk_file::read(std::uint64_t pos, std::span<std::byte> buf) {
   std::lock_guard<std::mutex> lock(mutex_);
-  if (!this->open_)
-    throw std::invalid_argument("File not open.");
 
   // Calculate the number of bytes available to read starting from 'pos'
   size_t availableBytes = buffer_.size() - pos;
 
-  // Check if the requested size exceeds the available data in the buffer
-  if (buf.size_bytes() > availableBytes) {
-    // Return 0 to indicate failure
-    return 0;
-  }
+  if (pos < 0 || pos >= buffer_.size())
+    return -22; // Invalid arg
 
   // Copy the data from 'buffer_' into 'buf' starting at the specified position
   // 'pos'
-  std::copy(buffer_.begin() + pos, buffer_.begin() + pos + buf.size_bytes(),
-            buf.begin());
+  size_t num_elements_to_copy =
+      std::min(static_cast<std::uint64_t>(buf.size()), buffer_.size() - pos);
+
+  // Perform the copy
+  std::copy_n(buffer_.begin() + pos, num_elements_to_copy, buf.begin());
 
   // Return the number of bytes read
-  return buf.size_bytes();
+  return num_elements_to_copy;
 }
 
-int file::saveBuffer() {
-  // Open file
-  std::ofstream outputFile("output.bin", std::ios::out | std::ios::binary);
-  if (!outputFile) {
-    std::cerr << "Failed to open the file for writing" << std::endl;
-    return 1;
+size_t disk_file::get_size() { return buffer_.size(); }
+
+// open_file functions
+
+int64_t open_file::write(std::span<const std::byte> buf) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  size_t bytes_written = pwrite_unlocked(buf, pos_);
+  pos_ += bytes_written;
+  return bytes_written;
+}
+
+int64_t open_file::read(std::span<std::byte> buf) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  size_t bytes_read = pread_unlocked(buf, pos_);
+  pos_ += bytes_read;
+  return bytes_read;
+}
+
+int64_t open_file::pwrite_unlocked(std::span<const std::byte> buf,
+                                   size_t offset) {
+  if (direct_) {
+    size_t bytes_written = disk_->write(offset, buf);
+  } else {
+    write_backlog_.push_back({{buf.begin(), buf.end()}, offset});
   }
-  // Write vector data to the file
-  outputFile.write(reinterpret_cast<const char *>(buffer_.data()),
-                   buffer_.size() * sizeof(std::byte));
-
-  // Close the file
-  outputFile.close();
-  return 0;
+  return buf.size();
 }
 
-void file::setState(bool state) { this->open_ = state; }
+int64_t open_file::pread_unlocked(std::span<std::byte> buf, size_t offset) {
+  flush_backlog();
+  size_t bytes_read = disk_->read(offset, buf);
+  return bytes_read;
+}
+
+int64_t open_file::pwrite(std::span<const std::byte> buf, size_t offset) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  return pwrite_unlocked(buf, offset);
+}
+
+int64_t open_file::pread(std::span<std::byte> buf, size_t offset) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  return pread_unlocked(buf, offset);
+}
+
+int64_t open_file::lseek(size_t offset, int whence) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  switch (whence) {
+  case SEEK_SET:
+    pos_ = offset;
+    break;
+  case SEEK_CUR:
+    pos_ += offset;
+    break;
+  case SEEK_END:
+    pos_ = disk_->get_size() + offset;
+    break;
+  default:
+    return -EINVAL;
+  }
+  return pos_;
+}
+
+void open_file::flush_backlog() {
+  if (direct_) {
+    return;
+  }
+
+  for (const write_req &req : write_backlog_) {
+    disk_->write(req.pos, req.bytes);
+  }
+  write_backlog_.clear();
+}
 
 } // namespace redstone::disk
