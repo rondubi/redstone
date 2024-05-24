1a2f90dc7d366164b35f7a49f149d04ca245e368
CutieOwl <kml11@live.com>
2024-05-24 12:34:20 -0700

    get time test

--- src/time/time_tests.cpp
+++ src/time/time_tests.cpp
@@ -25,9 +25,96 @@ TEST_P(TimeManagerTest, SleepTest) {
     EXPECT_NEAR(elapsed_real_time.count(), elapsed_sim_time_scaled.count(), 1000000);
 }
 
+TEST_P(TimeManagerTest, GetTimeTest) {
+    redstone::time::time_manager manager(scale);
+    auto start = std::chrono::system_clock::now();
+    auto sim_start = manager.clock_gettime();
+    auto end = std::chrono::system_clock::now();
+    auto sim_end = manager.clock_gettime();
+
+    auto elapsed_real_time = end - start;
+    auto elapsed_sim_time = sim_end - sim_start;
+    auto elapsed_sim_time_scaled = elapsed_sim_time / scale;
+
+    EXPECT_NEAR(elapsed_real_time.count(), elapsed_sim_time_scaled.count(), 1000000);
+}
+
+TEST_P(TimeManagerTest, SingleTimeout) {
+    redstone::time::time_manager tm(scale);
+    bool callback_called = false;
+    std::chrono::milliseconds sim_ms(100);
+    tm.set_timeout(sim_ms, [&callback_called]() {
+        callback_called = true;
+    });
+
+    std::chrono::milliseconds sleep_time(150);
+    std::this_thread::sleep_for(sleep_time / scale);
+    tm.process_timeouts();
+
+    EXPECT_TRUE(callback_called);
+}
+
+TEST_P(TimeoutTest, MultipleTimeouts) {
+    bool callback1_called = false;
+    bool callback2_called = false;
+
+    std::chrono::milliseconds sim_ms1(100);
+    tm.set_timeout(sim_ms1, [&callback1_called]() {
+        callback1_called = true;
+    });
+
+    std::chrono::milliseconds sim_ms2(200);
+    tm.set_timeout(sim_ms2, [&callback2_called]() {
+        callback2_called = true;
+    });
+
+    std::chrono::milliseconds sleep_time1(150);
+    std::this_thread::sleep_for(sleep_time1 / scale);
+    tm.process_timeouts();
+    EXPECT_TRUE(callback1_called);
+    EXPECT_FALSE(callback2_called);
+
+    std::chrono::milliseconds sleep_time2(100);
+    std::this_thread::sleep_for(sleep_time2 / scale);
+    tm.process_timeouts();
+    EXPECT_TRUE(callback2_called);
+}
+
+TEST_F(TimeoutTest, NoTimeouts) {
+    // Process timeouts when none are set
+    tm.process_timeouts();
+    // No expectations, just ensuring no crashes or unexpected behavior
+}
+
+TEST_F(TimeoutTest, OutOfOrderTimeouts) {
+    bool callback1_called = false;
+    bool callback2_called = false;
+
+    std::chrono::milliseconds sim_ms1(200);
+    tm.set_timeout(sim_ms1, [&callback2_called]() {
+        callback2_called = true;
+    });
+
+    std::chrono::milliseconds sim_ms2(100);
+    tm.set_timeout(sim_ms2, [&callback1_called]() {
+        callback1_called = true;
+    });
+
+    std::chrono::milliseconds sleep_time1(150);
+    std::this_thread::sleep_for(sleep_time1 / scale);
+    tm.process_timeouts();
+    EXPECT_TRUE(callback1_called);
+    EXPECT_FALSE(callback2_called);
+
+    std::chrono::milliseconds sleep_time2(100);
+    std::this_thread::sleep_for(sleep_time2 / scale);
+    tm.process_timeouts();
+    EXPECT_TRUE(callback2_called);
+}
+
 INSTANTIATE_TEST_CASE_P(ScaleValues, TimeManagerTest, ::testing::Values(1.0, 0.5, 2.0, 10.0, 100.0, 1000.0));
 
-TEST()
+INST
 
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
