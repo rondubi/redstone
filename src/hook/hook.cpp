01004d74cabac3b493209539f3080f3379f05627
Benjamin Friedman <bfriedma@stanford.edu>
2024-05-26 16:47:23 -0700

    Implement hook infrastructure

---
+++ src/hook/hook.cpp
@@ -0,0 +1,165 @@
+#include "redstone/hook/hook.hpp"
+#include <csignal>
+#include <cstdlib>
+#include <stdexcept>
+#include <sys/ptrace.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/user.h>
+#include <sys/wait.h>
+#include <system_error>
+#include <unistd.h>
+#include <vector>
+
+namespace redstone::hook {
+namespace {
+struct ptrace_syscall_info {
+  uint8_t op;                   /* Type of system call stop */
+  uint32_t arch;                /* AUDIT_ARCH_* value; see seccomp(2) */
+  uint64_t instruction_pointer; /* CPU instruction pointer */
+  uint64_t stack_pointer;       /* CPU stack pointer */
+  union {
+    struct {            /* op == PTRACE_SYSCALL_INFO_ENTRY */
+      uint64_t nr;      /* System call number */
+      uint64_t args[6]; /* System call arguments */
+    } entry;
+    struct {            /* op == PTRACE_SYSCALL_INFO_EXIT */
+      int64_t rval;     /* System call return value */
+      uint8_t is_error; /* System call error flag;
+                        Boolean: does rval contain
+                        an error value (-ERRCODE) or
+                        a nonerror return value? */
+    } exit;
+    struct {             /* op == PTRACE_SYSCALL_INFO_SECCOMP */
+      uint64_t nr;       /* System call number */
+      uint64_t args[6];  /* System call arguments */
+      uint32_t ret_data; /* SECCOMP_RET_DATA portion
+                         of SECCOMP_RET_TRACE
+                         return value */
+    } seccomp;
+  };
+};
+
+struct child {
+  pid_t pid;
+
+  void wait(int *status, int options = 0) {
+    if (0 > waitpid(pid, status, options))
+      throw std::system_error{errno, std::generic_category(), "waitpid failed"};
+  }
+
+  template <typename T, typename U>
+  void ptrace(enum __ptrace_request req, T addr, U data) {
+    if (0 > ::ptrace(req, pid, addr, data))
+      throw std::system_error{errno, std::generic_category(), "ptrace failed"};
+  }
+
+  void ptrace_syscall() { ptrace(PTRACE_SYSCALL, 0, 0); }
+
+  void ptrace_get_regs(user_regs_struct *regs) {
+    ptrace(PTRACE_GETREGS, 0, regs);
+  }
+
+  void ptrace_set_regs(const user_regs_struct &regs) {
+    ptrace(PTRACE_SETREGS, 0, &regs);
+  }
+
+  void ptrace_set_options(int options) {
+    ptrace(PTRACE_SETOPTIONS, 0, options);
+  }
+
+  void ptrace_get_syscall_info(ptrace_syscall_info *info) {
+    ptrace(PTRACE_GET_SYSCALL_INFO, sizeof(*info), info);
+  }
+};
+
+child spawn(const command &cmd) {
+  if (cmd.args.at(0) != cmd.path) {
+    throw std::invalid_argument{
+        "spawn: argv[0] must be the same as the provided path"};
+  }
+
+  std::vector<char *> argv;
+
+  for (auto &arg : cmd.args) {
+    argv.push_back(const_cast<char *>(arg.c_str()));
+  }
+  argv.push_back(nullptr);
+
+  pid_t pid = fork();
+  child c{pid};
+
+  if (pid < 0) {
+    throw std::system_error{errno, std::generic_category(),
+                            "failed to fork process"};
+  }
+
+  if (0 < pid) {
+    return c;
+  }
+
+  c.ptrace(PTRACE_TRACEME, 0, 0);
+  std::raise(SIGSTOP);
+
+  execvp(cmd.path.c_str(), argv.data());
+  throw std::system_error{errno, std::generic_category(), "execvp failed"};
+}
+
+} // namespace
+
+void run_command(const command &cmd, handlers &handlers) {
+  int status;
+  user_regs_struct regs;
+  ptrace_syscall_info info;
+
+  auto child = spawn(cmd);
+
+  child.wait(nullptr);
+  child.ptrace_set_options(PTRACE_O_TRACESYSGOOD);
+
+  for (;;) {
+    child.ptrace_syscall();
+    child.wait(&status);
+
+    if (WIFSIGNALED(status)) {
+      handlers.signaled(WTERMSIG(status));
+      return;
+    }
+    if (WIFEXITED(status)) {
+      handlers.exited(WEXITSTATUS(status));
+      return;
+    }
+    if (WIFSTOPPED(status)) {
+      int sig = WSTOPSIG(status);
+
+      if (sig != (0x80 | SIGTRAP)) {
+        handlers.stopped(sig);
+      }
+    }
+
+    child.ptrace_get_syscall_info(&info);
+
+    if (info.op != PTRACE_SYSCALL_INFO_ENTRY) {
+      continue;
+    }
+
+    auto result = handlers.maybe_handle(info.entry.nr, info.entry.args);
+
+    if (!result.has_value()) {
+      continue;
+    }
+
+    child.ptrace_get_regs(&regs);
+    regs.orig_rax = SYS_gettid;
+    regs.rax = SYS_gettid;
+    child.ptrace_set_regs(regs);
+
+    child.ptrace_syscall();
+    child.wait(NULL);
+
+    regs.orig_rax = *result;
+    regs.rax = *result;
+    child.ptrace_set_regs(regs);
+  }
+}
+} // namespace redstone::hook
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,219 @@
-#include <fmt/format.h>
+#include "redstone/hook/hook.hpp"
 
-#include "redstone/disk/disk.hpp"
-#include "redstone/net/net.hpp"
-#include "redstone/time/time.hpp"
+#include <cstdio>
+#include <sys/syscall.h>
 
-int main() { fmt::println("Hello, world!"); }
+struct logging_handler : redstone::hook::handlers {
+  std::optional<int> maybe_handle(int sysno,
+                                  std::span<const uint64_t, 6> args) override {
+    if (sysno == SYS_write && args[0] == 1) {
+      uintptr_t ptr = args[1];
+      size_t len = args[2];
+      printf("write stdout ptr = %p, len = %zu\n", (void *)ptr, len);
+      return len;
+    }
+    printf("syscall: %d\n", sysno);
+    return std::nullopt;
+  }
+
+  void exited(int status) override { printf("exited: %d\n", status); }
+};
+
+int main() {
+  redstone::hook::command cmd;
+  cmd.path = "../test";
+  cmd.args.push_back(cmd.path);
+
+  logging_handler handler;
+  redstone::hook::run_command(cmd, handler);
+}
+
+// #include <cassert>
+// #include <csignal>
+// #include <cstdint>
+// #include <cstdio>
+// #include <cstdlib>
+// #include <map>
+// #include <optional>
+// #include <set>
+// #include <span>
+// #include <sys/ptrace.h>
+// #include <sys/syscall.h>
+// #include <sys/user.h>
+// #include <sys/wait.h>
+// #include <system_error>
+// #include <unistd.h>
+// #include <vector>
+
+// #include "redstone/net/net.hpp"
+// #include "redstone/time/time.hpp"
+
+// void check_result(long result) {
+//   if (result < 0) {
+//     throw std::system_error{errno, std::generic_category()};
+//   }
+// }
+
+// struct ptrace_syscall_info {
+//   uint8_t op;                   /* Type of system call stop */
+//   uint32_t arch;                /* AUDIT_ARCH_* value; see seccomp(2) */
+//   uint64_t instruction_pointer; /* CPU instruction pointer */
+//   uint64_t stack_pointer;       /* CPU stack pointer */
+//   union {
+//     struct {            /* op == PTRACE_SYSCALL_INFO_ENTRY */
+//       uint64_t nr;      /* System call number */
+//       uint64_t args[6]; /* System call arguments */
+//     } entry;
+//     struct {            /* op == PTRACE_SYSCALL_INFO_EXIT */
+//       int64_t rval;     /* System call return value */
+//       uint8_t is_error; /* System call error flag;
+//                         Boolean: does rval contain
+//                         an error value (-ERRCODE) or
+//                         a nonerror return value? */
+//     } exit;
+//     struct {             /* op == PTRACE_SYSCALL_INFO_SECCOMP */
+//       uint64_t nr;       /* System call number */
+//       uint64_t args[6];  /* System call arguments */
+//       uint32_t ret_data; /* SECCOMP_RET_DATA portion
+//                          of SECCOMP_RET_TRACE
+//                          return value */
+//     } seccomp;
+//   };
+// };
+
+// using handler = int (*)(uint64_t[6]);
+
+// struct syscall_definitions {
+//   std::set<int> transparent;
+//   std::map<int, handler> overrides;
+// };
+
+// std::map<int, handler> build_overrides() { return {}; }
+
+// const std::map<int, handler> &overrides() {
+//   static const auto map = build_overrides();
+//   return map;
+// }
+
+// handler get_override(int sysno) {
+//   auto map = overrides();
+//   return map[sysno];
+// }
+
+// enum class syscall_kind {
+//   handle,
+//   transparent,
+//   unknown,
+// };
+
+// struct syscall_hooks {
+//   std::optional<int> handle(int sysno, std::span<const uint64_t, 6> args) {
+//     printf("syscall: %d\n", sysno);
+
+//     if (sysno == SYS_write && args[0] == 1) {
+//       uintptr_t ptr = args[1];
+//       size_t len = args[2];
+//       printf("write stdout ptr = %p, len = %zu\n", (void *)ptr, len);
+//       return len;
+//     }
+
+//     return std::nullopt;
+//   }
+// };
+
+// void child() {
+//   fprintf(stderr, "enter!\n");
+//   check_result(ptrace(PTRACE_TRACEME));
+
+//   kill(getpid(), SIGSTOP);
+
+//   close(4);
+//   fprintf(stderr, "hello!\n");
+//   fflush(stderr);
+
+//   char *argv[] = {"../test", nullptr};
+//   check_result(execv(argv[0], argv));
+// }
+
+// void parent(pid_t pid) {
+//   std::set<int> unhandled;
+
+//   syscall_hooks hooks;
+
+//   waitpid(pid, NULL, 0);
+//   check_result(ptrace(PTRACE_SETOPTIONS, pid, 0, PTRACE_O_TRACESYSGOOD));
+
+//   for (;;) {
+//     check_result(ptrace(PTRACE_SYSCALL, pid, 0, 0));
+//     int status;
+//     waitpid(pid, &status, 0);
+
+//     if (WIFSTOPPED(status)) {
+//       switch (WSTOPSIG(status) & 0x7f) {
+//       case SIGSEGV:
+//         abort();
+//         break;
+//       case SIGTRAP:
+//         break;
+//       default:
+//         // printf("unknown signal %d\n", WSTOPSIG(status) & 0x7f);
+//         break;
+//       }
+//     } else if (WIFEXITED(status)) {
+//       return;
+//     }
+
+//     // printf("signal %d\n", WSTOPSIG(status));
+//     // printf("signaled := %d\n", WIFSIGNALED(status));
+//     // printf("exited := %d\n", WIFEXITED(status));
+//     // printf("stopped := %d\n", WIFSTOPPED(status));
+
+//     // if (WIFSTOPPED(status) && (WSTOPSIG(status) & 0x80) != 0)
+//     //   break;
+
+//     struct ptrace_syscall_info info;
+
+//     check_result(ptrace(PTRACE_GET_SYSCALL_INFO, pid, sizeof(info), &info));
+
+//     if (info.op != PTRACE_SYSCALL_INFO_ENTRY) {
+//       continue;
+//     }
+
+//     // printf("SyscallInfo {\n\top: %d,\n\tnumber: %lu\n}\n", info.op,
+//     //        info.entry.nr);
+
+//     auto result = hooks.handle(info.entry.nr, info.entry.args);
+
+//     if (!result.has_value()) {
+//       continue;
+//     }
+
+//     user_regs_struct regs;
+
+//     check_result(ptrace(PTRACE_GETREGS, pid, 0, &regs));
+//     regs.rax = SYS_gettid;
+//     regs.orig_rax = SYS_gettid;
+//     check_result(ptrace(PTRACE_SETREGS, pid, 0, &regs));
+
+//     check_result(ptrace(PTRACE_SYSCALL, pid, 0, 0));
+//     waitpid(pid, &status, 0);
+
+//     // check_result(ptrace(PTRACE_GETREGS, pid, 0, &regs));
+//     regs.rax = result.value();
+//     check_result(ptrace(PTRACE_SETREGS, pid, 0, &regs));
+//   }
+// }
+
+// int main() {
+//   // char *argv[] = {"../test", nullptr};
+//   // check_result(execv(argv[0], argv));
+
+//   pid_t pid = fork();
+//   if (pid < 0) {
+//     throw std::system_error{errno, std::generic_category()};
+//   } else if (pid == 0) {
+//     child();
+//   } else {
+//     parent(pid);
+//   }
+// }
