baa48bd29f9aa7a0401d40c566eaea2938e20cf1
Benjamin Friedman <bfriedma@stanford.edu>
2024-06-06 12:33:04 -0700

    Implement stdout_file_descriptor for testing

--- src/sim/file_descriptor.cpp
+++ src/sim/file_descriptor.cpp
@@ -1,6 +1,11 @@
 #include "sim/file_descriptor.hpp"
+#include "sys/file.hpp"
 
 #include <cassert>
+#include <cstddef>
+#include <memory>
+#include <spdlog/spdlog.h>
+#include <vector>
 
 namespace redstone::sim {
 std::shared_ptr<file_descriptor> file_descriptor_table::get(int fd) {
@@ -50,5 +55,49 @@ int file_descriptor_table::insert(std::shared_ptr<file_descriptor> fildes) {
   assert(!slot);
   slot = std::move(fildes);
   return fd;
+}
+
+namespace {
+class stdout_file_descriptor final : public file_descriptor {
+public:
+  stdout_file_descriptor() = default;
+
+  std::int64_t write(tl::function_ref<int(std::span<std::byte>)> load,
+                     const std::size_t bytes) final {
+    std::vector buf{8192, std::byte{}};
+
+    std::size_t done = 0;
+
+    while (done < bytes) {
+      std::span tmp = buf;
+
+      if (tmp.size_bytes() > bytes) {
+        tmp = tmp.subspan(0, bytes);
+      }
+
+      spdlog::trace("f1");
+      auto res = load(tmp);
+      spdlog::trace("f2");
+      if (res < 0) {
+        return res;
+      }
+
+      std::fwrite(tmp.data(), 1, tmp.size_bytes(), stdout);
+
+      done += tmp.size_bytes();
+    }
+
+    return done;
+  };
+
+private:
+  sys::file f_;
+};
+} // namespace
+
+std::shared_ptr<file_descriptor> stdout_fd() {
+  static std::shared_ptr<stdout_file_descriptor> shared =
+      std::make_shared<stdout_file_descriptor>();
+  return shared;
 }
 } // namespace redstone::sim
--- src/sim/file_descriptor.hpp
+++ src/sim/file_descriptor.hpp
@@ -1,13 +1,30 @@
 #pragma once
 
+#include <cerrno>
+#include <cstddef>
 #include <memory>
 #include <mutex>
+#include <span>
+#include <tl/function_ref.hpp>
 #include <unordered_map>
 #include <vector>
 
 namespace redstone::sim {
 class file_descriptor {
 public:
+  virtual ~file_descriptor() = default;
+
+  virtual std::int64_t
+  read(tl::function_ref<int(std::span<const std::byte>)> store,
+       std::size_t bytes) {
+    return -EINVAL;
+  }
+
+  virtual std::int64_t write(tl::function_ref<int(std::span<std::byte>)> load,
+                             std::size_t bytes) {
+    return -EINVAL;
+  };
+
 private:
 };
 
@@ -15,7 +32,7 @@ class file_descriptor_table {
 public:
   file_descriptor_table() = default;
 
-  bool is_simulated(int fd) const { return (fd & simulated) != 0; }
+  static bool is_simulated(int fd) { return (fd & simulated) != 0; }
 
   std::shared_ptr<file_descriptor> get(int fd);
   int close(int fd);
@@ -31,4 +48,6 @@ private:
   std::unordered_map<int, std::shared_ptr<file_descriptor>> table_;
   std::vector<int> unused_;
 };
+
+std::shared_ptr<file_descriptor> stdout_fd();
 } // namespace redstone::sim
