38f05067284a319d8b36751646366cf7f16bc747
Benjamin Friedman <bfriedma@stanford.edu>
2024-06-08 11:56:06 -0700

    Fix determinism check script

--- determinism.py
+++ determinism.py
@@ -1,19 +1,31 @@
 import subprocess
 import hashlib
+import time
 
 RUNS = 100
-args = ["./build/redstone", "sim.toml"]
+args = ["./build/redstone", "demo_repro3.toml"]
 
 sha = None
 
-for _ in range(RUNS):
+runs = []
+
+
+
+for i in range(RUNS):
     result = subprocess.run(args, stdout=subprocess.PIPE)
     result.check_returncode()
 
     print("finished run!")
 
-    iter_sha = hashlib.sha256(result.stdout)
+    runs.append(result.stdout)
+
+    with open(f"runs/run{i}.txt", "wb") as f:
+        _ = f.write(result.stdout)
+
+    iter_sha = hashlib.sha256(result.stdout).digest()
     if sha is None:
         sha = iter_sha
     if sha != iter_sha:
+        print(sha, iter_sha)
         print("run had different sha")
+    time.sleep(1)
